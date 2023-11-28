e025edc5f32ff9b4cfaa4797daf9359330c7e2bc
ajayyadav <dev.miajay@gmail.com>
2023-11-28 18:53:58 +0530

    - Splash Screen

--- app/src/main/java/app/apex/com/ui/SplashActivity.kt
+++ app/src/main/java/app/apex/com/ui/SplashActivity.kt
@@ -8,7 +8,7 @@ import app.apex.com.R
 
 class SplashActivity : AppCompatActivity() {
 
-    private val SPLASH_TIME_OUT: Long = 1000 // 3 seconds
+    private val SPLASH_TIME_OUT: Long = 3000 // 3 seconds
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
